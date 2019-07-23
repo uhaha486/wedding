cb349ad2906c5d0a75892c9cd66c713260abde9e
uhaha486 <52910600+uhaha486@users.noreply.github.com>
2019-07-23 16:55:33 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(document).ready(function(){
+        $('.bxslider').bxSlider({
+            pager:false,
+            auto:true,
+            mode:'fade',
+            speed:3000,
+    
+        });
+      });
+});
